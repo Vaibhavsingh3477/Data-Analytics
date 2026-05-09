89616386690144fae9939e1acea267290d04d5dc
Vaibhav Singh <vaibhavsinghcs054@gmail.com>
2026-05-09 23:28:08 +0530

    Fixed the File Structure

---
+++ Introduction to PYTHON/package/__init__.py
@@ -0,0 +1,9 @@
+'''
+This is a simple package for demonstration purposes.
+'''
+'''
+__init__.py is a special file in Python that is used to indicate that the directory it is in should be treated as a package. 
+It can be an empty file, but it can also contain initialization code for the package. 
+When you import a package, the code in __init__.py is executed, 
+allowing you to set up any necessary variables, functions, or classes that should be available when the package is imported.
+'''
---
+++ Introduction to PYTHON/package/maths.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+def calculate_distance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
